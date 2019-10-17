38bb9fd3fb5b3330d083e4e011a84efcae607cb3
Ali <aliseylaneh@live.com>
2019-10-17 23:36:46 +0330

    Initial Project Add

---
+++ src/main/java/com/aliseylaneh/ascpetclinic/AscPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.aliseylaneh.ascpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AscPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aliseylaneh/ascpetclinic/AscPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliseylaneh.ascpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AscPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
