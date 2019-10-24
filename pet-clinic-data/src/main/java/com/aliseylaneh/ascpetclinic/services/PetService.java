44c0d839832e27dcb9a07d37a5091f0a196feeba
Ali <aliseylaneh@live.com>
2019-10-25 00:50:41 +0330

    Refactoring services by using common crud interface.

--- pet-clinic-data/src/main/java/com/aliseylaneh/ascpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/aliseylaneh/ascpetclinic/services/PetService.java
@@ -2,12 +2,6 @@ package com.aliseylaneh.ascpetclinic.services;
 
 import com.aliseylaneh.ascpetclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
