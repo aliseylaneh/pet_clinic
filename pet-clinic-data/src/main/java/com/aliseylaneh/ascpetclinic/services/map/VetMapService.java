34ba515c9f8aca0d566448171b4e064f9b522861
aliseylaneh <aliseylaneh@gmail.com>
2019-10-25 15:54:25 +0330

    Implementation map based services.

---
+++ pet-clinic-data/src/main/java/com/aliseylaneh/ascpetclinic/services/map/VetMapService.java
@@ -0,0 +1,33 @@
+package com.aliseylaneh.ascpetclinic.services.map;
+
+import com.aliseylaneh.ascpetclinic.model.Vet;
+import com.aliseylaneh.ascpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetMapService extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
