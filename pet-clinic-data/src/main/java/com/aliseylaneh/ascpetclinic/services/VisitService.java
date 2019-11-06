b158776401cc338fe0f6ef64e33d28c66f1085a5
aliseylaneh <aliseylaneh@gmail.com>
2019-11-06 23:18:51 +0330

    Map service implementation for Visit

---
+++ pet-clinic-data/src/main/java/com/aliseylaneh/ascpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.aliseylaneh.ascpetclinic.services;
+
+import com.aliseylaneh.ascpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/aliseylaneh/ascpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,36 @@
+package com.aliseylaneh.ascpetclinic.services.map;
+
+import com.aliseylaneh.ascpetclinic.model.Visit;
+import com.aliseylaneh.ascpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null ||
+                visit.getPet().getId() == null || visit.getPet().getOwner().getId() == null)
+            throw new RuntimeException("Invalid Visit");
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
