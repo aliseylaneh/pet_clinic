06bd1d6f6ba02fb1149b20b377f5be5e4367dcce
aliseylaneh <aliseylaneh@gmail.com>
2019-11-29 23:24:47 +0330

    Added VisitController.

---
+++ pet-clinic-web/src/main/java/com/aliseylaneh/ascpetclinic/controllers/VisitController.java
@@ -0,0 +1,57 @@
+package com.aliseylaneh.ascpetclinic.controllers;
+
+import com.aliseylaneh.ascpetclinic.model.Pet;
+import com.aliseylaneh.ascpetclinic.model.Visit;
+import com.aliseylaneh.ascpetclinic.services.PetService;
+import com.aliseylaneh.ascpetclinic.services.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+public class VisitController {
+    private static final String VIEWS_VISIT_CREATE_OR_UPDATE_FORM = "pets/createOrUpdateVisitForm";
+
+    private final VisitService visitService;
+    private final PetService petService;
+
+    public VisitController(VisitService visitService, PetService petService) {
+        this.visitService = visitService;
+        this.petService = petService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable Long petId, Model model) {
+        Pet pet = petService.findById(petId);
+        model.addAttribute("pet", pet);
+        Visit visit = new Visit();
+        pet.getVisits().add(visit);
+        visit.setPet(pet);
+        return visit;
+    }
+
+    @GetMapping("/owners/*/pets/{petId}/visits/new")
+    public String initNewVisitForm(@PathVariable Long petId, Model model) {
+        return VIEWS_VISIT_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String processNewVisitForm(@Valid Visit visit, BindingResult result) {
+        if (result.hasErrors()) {
+            return VIEWS_VISIT_CREATE_OR_UPDATE_FORM;
+        } else {
+            visitService.save(visit);
+            return "redirect:/owners/{ownerId}";
+
+        }
+    }
+}
