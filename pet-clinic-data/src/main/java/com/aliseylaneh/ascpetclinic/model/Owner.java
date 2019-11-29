e056d8bf0fd943ccb52fa85bad8db4adcd661ac4
aliseylaneh <aliseylaneh@gmail.com>
2019-11-29 22:13:09 +0330

    Added add and update ability to to pet controller. Added PetTypeFormatter from spring guys to handle the view layer error

--- pet-clinic-data/src/main/java/com/aliseylaneh/ascpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/aliseylaneh/ascpetclinic/model/Owner.java
@@ -1,6 +1,9 @@
 package com.aliseylaneh.ascpetclinic.model;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -9,7 +12,6 @@ import java.util.Set;
 @Setter
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
@@ -20,7 +22,7 @@ public class Owner extends Person {
         this.address = address;
         this.city = city;
         this.telephone = telephone;
-        this.pets = pets;
+        if (pets != null) this.pets = pets;
     }
 
     @Column(name = "address")
@@ -34,5 +36,21 @@ public class Owner extends Person {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
+
+    public Pet getPet(String name) {
+        return getPet(name, false);
+    }
+
+    public Pet getPet(String name, boolean ignoreNew) {
+        for (Pet pet : pets) {
+            if (!ignoreNew || !pet.isNew()) {
+                String compName = pet.getName().toLowerCase();
+                if (compName.equals(name.toLowerCase())) {
+                    return pet;
+                }
+            }
+        }
+        return null;
+    }
 
 }
